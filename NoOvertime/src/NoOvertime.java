916cd911425a1af40f14eed991a4dba9547d00fb
SeonHyungJO <SeonHyungJo@users.noreply.github.com>
2017-06-13 17:07:48 +0900

    update Question

---
+++ NoOvertime/src/NoOvertime.java
@@ -0,0 +1,13 @@
+class NoOvertime {
+	public int noOvertime(int no, int[] works) {
+		int result = 0;
+		// 야근 지수를 최소화 하였을 때의 야근 지수는 몇일까요?
+
+		return result;
+	}
+	public static void main(String[] args) {
+		NoOvertime c = new NoOvertime();
+		int []test = {4,3,3};
+		System.out.println(c.noOvertime(4,test));
+	}
+}
