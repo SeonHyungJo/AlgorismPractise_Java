a64fca143418c8fea48a8f1a9ce531b736a08653
SeonHyungJO <SeonHyungJo@users.noreply.github.com>
2017-06-14 13:36:01 +0900

    update Question

---
+++ Caesar/src/Caesar.java
@@ -0,0 +1,13 @@
+class Caesar {
+	String caesar(String s, int n) {
+		String result = "";
+		// 함수를 완성하세요.
+
+		return result;
+	}
+
+	public static void main(String[] args) {
+		Caesar c = new Caesar();
+		System.out.println("s는 'a B z', n은 4인 경우: " + c.caesar("a B z", 4));
+	}
+}
